a7ce210060ffcdd41cbbe9185ca4d40b76d447df
Pedro Henrique Moura <pedronerymoura@gmail.com>
2023-11-26 15:38:14 -0300

    Update consultas.php

--- ProjetoDentCare/consultas.php
+++ ProjetoDentCare/consultas.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-include_once('backend/conexao.php');
+include_once('backend/conexao.php'); 
 
 if (!isset($_SESSION['login']) || !isset($_SESSION['senha'])) {
     unset ($_SESSION['login']);
